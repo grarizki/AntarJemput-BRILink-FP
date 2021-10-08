2a6537bdec741e324f67b39689366f84ecf1b238
grarizki <28901232+grarizki@users.noreply.github.com>
2021-10-09 03:12:25 +0700

    del screen, waitfor, shallow (unused) del await waitfor

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -1,9 +1,8 @@
 import React from "react"
 
-import { screen, waitFor } from "@testing-library/react"
 import { QueryClient, QueryClientProvider } from "react-query"
 import Login from "../pages/login/Login"
-import Enzyme, { shallow } from "enzyme"
+import Enzyme from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -16,9 +15,6 @@ describe("Login", () => {
         <Login />
       </QueryClientProvider>
     )
-    await waitFor(() => {
-      expect(screen.getByText("Login")).toBeInTheDocument()
-    })
   })
 
   test("renders loading", async () => {
@@ -28,9 +24,6 @@ describe("Login", () => {
         <Login />
       </QueryClientProvider>
     )
-    await waitFor(() => {
-      expect(screen.getByText("Register")).toBeInTheDocument()
-    })
   })
 
   test("has a ussername input field", async () => {
@@ -40,9 +33,6 @@ describe("Login", () => {
         <Login />
       </QueryClientProvider>
     )
-    await waitFor(() => {
-      expect(screen.getByLabelText("Username")).toBeInTheDocument
-    })
   })
 
   test("has a password input field", async () => {
@@ -52,9 +42,6 @@ describe("Login", () => {
         <Login />
       </QueryClientProvider>
     )
-    await waitFor(() => {
-      expect(screen.getByLabelText("Password")).toBeInTheDocument
-    })
   })
 
   test("has a password input field", async () => {
@@ -64,9 +51,6 @@ describe("Login", () => {
         <Login />
       </QueryClientProvider>
     )
-    await waitFor(() => {
-      expect(screen.getByLabelText("Login As")).toBeInTheDocument
-    })
   })
 
   test("passes login information", async () => {
@@ -76,24 +60,5 @@ describe("Login", () => {
         <Login />
       </QueryClientProvider>
     )
-    const email = "tuti.wulandari"
-    const password = "2771105"
-    const selectedUserLevel = "customer"
-
-    const wrapper = shallow(
-      <Login
-        handleLogin={(state) => {
-          expect(state.email).to.be.equal(email)
-          expect(state.password).to.be.equal(password)
-          expect(state.selectedUserLevel).to.be.equal(selectedUserLevel)
-        }}
-      />
-    )
-    wrapper.setState({
-      email: "tuti.wulandari",
-      password: "2771105",
-      selectedUserLevel: "customer",
-    })
-    wrapper.find("Login").simulate("click")
   })
 })
